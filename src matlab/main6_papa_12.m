f3f7e8c22b06f4968e410d05d3e6b615a716ff00
Vlad Karpushin <karpushin@ngs.ru>
2018-08-17 12:23:31 +0700

    added interctively input

--- src matlab/main6_papa_12.m
+++ src matlab/main6_papa_12.m
@@ -1,8 +1,8 @@
 % 2016-01-04 
 % реализован алгоритм удаления трафаретной сетки (растра) при газетной печати
 % background cell removing algorithm
-% 2018-08-16
-% ###added user interactivness
+% 2018-08-17
+% added user interactivness
 
 close all,clc,clear all;
 
@@ -19,59 +19,59 @@ imgA = imadjust(imgA);
 figure, imshow(imgA);
 title('Original image');
 
-img_fft = fft2(imgA);   %spectrum 
-img_fft(1,1) = 0;   %removing of constant component
+%****************************
+%*****PSD calculation********
+%****************************
+
+img_fft = fft2(imgA);           %spectrum 
+img_fft(1,1) = 0;               %removing of constant component
 imgB = img_fft.*conj(img_fft);  %Power spectrum density
 imgC = fftshift(255*(imgB -min(min(imgB))) /(max(max(imgB)) - min(min(imgB))));
 
 figure; imshow(imgC);
 title('Power spectrum density');
-imwrite(imgC,strcat(strFolder,'output\Power_spectrum_density.bmp'));
 
 % user input:
+%4-element vector of the form [xmin ymin width height]. The initial size of the rectangle is width-by-height pixels. The upper-left corner of the rectangle is at the (x,y) coordinate (xmin,ymin).
 h = imrect();
-pos = getPosition(h);
+pos = getPosition(h);       
 
 [h w] = size(imgC);
 imgD = ones([h w]);
 
-
 %Array of noise. Enough first eight components
-x1 = [1620 1618 1211 1212 1416 1822 1414 1007 1409 1620 1726 1420 1212 1105 1622 1824 2028 2026 1821 1618 1208 1007 805  806 1010 1215];
-y1 = [ 912 1222 1221  911 757  1068 1377 1066 890  1009 1068 1242 1123 1064 602  757  914  1225 1378 1532 1532 1375 1220 907 754  602];
+% x1 = [1620 1618 1211 1212 1416 1822 1414 1007 1409 1620 1726 1420 1212 1105 1622 1824 2028 2026 1821 1618 1208 1007 805  806 1010 1215];
+% y1 = [ 912 1222 1221  911 757  1068 1377 1066 890  1009 1068 1242 1123 1064 602  757  914  1225 1378 1532 1532 1375 1220 907 754  602];
 
-n = 25; %size of removing spot. 30 - optimum
-for k = 1:size(x1') % k - index of noise component
-    for i =-n:n
-         for j =-n:n
-                imgD(y1(k)+i,x1(k)+j) = 0; %imgD(row, colomn), this is why imgD(y,x) rather than (x,y)
-         end;
-     end;
-end;
+xmin = pos(1);
+ymin = pos(2);
+width = pos(3);
+height = pos(4);
+
+imgD(ymin:ymin+height, xmin:xmin+width) = 0; %imgD(row, colomn), this is why imgD(y,x) rather than (x,y)
 figure; imshow((imgC+imgD)/2);
 title('Power spectrum density + mask');
 
+%**********************
+%*****Filtering********
+%**********************
+
 imgE = ifft2(fftshift(imgD).*img_fft); %filtering
+max(max(abs(real(imgE))))
+max(max(abs(imag(imgE))))
 imgE = real(imgE);  %it needs to get real part because imgE is complex value
 imgE = uint8(255*(imgE - min(min(imgE))) /(max(max(imgE)) - min(min(imgE))));
 imgE = imadjust(imgE);
+
+%*******************
+%*****Output********
+%*******************
+
+
 figure; imshow(imgE);
 title('Final image');
 
 figure, imshowpair(imgA, imgE,'montage');
 imwrite(imgA,strcat(strFolder,'output\input.bmp'));
 imwrite(imgE,strcat(strFolder,'output\output.bmp'));
-
-
-% imgAA = imadjust(imgA,[70 217]/255);
-% figure; imshow(imgAA);
-% imwrite(imgAA,strcat(strFolder,'output\Contrasted_original_image.bmp'));
-% title('Contrasted original image');
-
-% imgFFF = imadjust(imgFF,[58 170]/255);
-% figure; imshow(imgFFF);
-% imwrite(imgFFF,strcat(strFolder,'output\Contrasted_Final_image.bmp'));
-% title('Contrasted Final image');
-
-% figure, imshowpair(imgAA, imgFFF,'montage');
-% figure, imshowpair(imgA, imgFF,'montage');
+imwrite(imgC,strcat(strFolder,'output\Power_spectrum_density.bmp'));
